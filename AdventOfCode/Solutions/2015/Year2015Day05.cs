b83003cc5d50e1cddb196cc59fc9af5a3ce4f13a
ichbineinnerd <xximtilixx@gmail.com>
2020-03-04 20:26:24 +0100

    2015, 05 and 06

---
+++ AdventOfCode/Solutions/2015/Year2015Day05.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Solutions._2015
+{
+    class Year2015Day05 : Solution
+    {
+        public override string Part1(string input)
+        {
+            string[] strings = input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int count = 0;
+
+            foreach (string s in strings)
+            {
+                if (s.IndexOf("ab") >= 0 || s.IndexOf("cd") >= 0 || s.IndexOf("pq") >= 0 || s.IndexOf("xy") >= 0)
+                    continue;
+                int vowelCount = s[s.Length - 1] == 'a' || s[s.Length - 1] == 'e' || s[s.Length - 1] == 'i' || s[s.Length - 1] == 'o' || s[s.Length - 1] == 'u' ? 1 : 0;
+                bool hasDoubleLetter = false;
+                for (int i = 0; i < s.Length - 1; i++)
+                {
+                    if (s[i] == s[i + 1])
+                        hasDoubleLetter = true;
+                    if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                        vowelCount++;
+                }
+                if (vowelCount < 3 || !hasDoubleLetter)
+                    continue;
+
+                count++;
+            }
+
+            return count.ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            string[] strings = input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int count = 0;
+
+            foreach (string s in strings)
+            {
+                bool hasDoubleLetter = false;
+                for (int i = 0; i < s.Length - 2; i++)
+                {
+                    if (s[i] == s[i + 2])
+                        hasDoubleLetter = true;
+                }
+
+                bool canBreak = false;
+                //not efficient, but meh, there arent that many strings
+                for (int i = 0; i < s.Length - 1; i++)
+                {
+                    string twoLetters = s.Substring(i, 2);
+
+                    for (int j = i + 2; j < s.Length - 1; j++)
+                    {
+                        if (j == i || j == i - 1 || j == i + 1)
+                            continue;
+                        if (s.Substring(j, 2) == twoLetters)
+                        {
+                            canBreak = true;
+                            break;
+                        }
+                    }
+                    if (canBreak)
+                        break;
+                }
+
+                if (!hasDoubleLetter || !canBreak)
+                    continue;
+
+                count++;
+            }
+
+            return count.ToString();
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/2015/Year2015Day06.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Solutions._2015
+{
+    class Year2015Day06 : Solution
+    {
+        public override string Part1(string input)
+        {
+            bool[] grid = new bool[1_000_000];
+            Array.Fill(grid, false);
+
+            foreach (string s in input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                int operation = -1;
+                switch (s.Substring(0, 7))
+                {
+                    case "turn on":
+                        operation = 0;
+                        break;
+                    case "turn of":
+                        operation = 1;
+                        break;
+                    case "toggle ":
+                        operation = 2;
+                        break;
+                    default:
+                        throw new Exception("unrecognized instruction: " + s.Substring(0, 7));
+                }
+
+                string[] parts = s.Split(' ');
+                string[] point1 = parts[parts.Length - 3].Split(',');
+                string[] point2 = parts[parts.Length - 1].Split(',');
+
+                (int x, int y) firstPoint = (int.Parse(point1[0]), int.Parse(point1[1]));
+                (int x, int y) secondPoint = (int.Parse(point2[0]), int.Parse(point2[1])); ;
+
+                if (operation == 0)
+                {
+                    for (int i = firstPoint.x; i <= secondPoint.x; i++)
+                    {
+                        for (int j = firstPoint.y; j <= secondPoint.y; j++)
+                        {
+                            grid[j * 1000 + i] = true;
+                        }
+                    }
+                }else if (operation == 1)
+                {
+                    for (int i = firstPoint.x; i <= secondPoint.x; i++)
+                    {
+                        for (int j = firstPoint.y; j <= secondPoint.y; j++)
+                        {
+                            grid[j * 1000 + i] = false;
+                        }
+                    }
+                }else
+                {
+                    for (int i = firstPoint.x; i <= secondPoint.x; i++)
+                    {
+                        for (int j = firstPoint.y; j <= secondPoint.y; j++)
+                        {
+                            grid[j * 1000 + i] = !grid[j * 1000 + i];
+                        }
+                    }
+                }
+            }
+
+            return grid.Count(x => x).ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            int[] grid = new int[1_000_000];
+            Array.Fill(grid, 0);
+
+            foreach (string s in input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                int operation = -1;
+                switch (s.Substring(0, 7))
+                {
+                    case "turn on":
+                        operation = 1;
+                        break;
+                    case "turn of":
+                        operation = -1;
+                        break;
+                    case "toggle ":
+                        operation = 2;
+                        break;
+                    default:
+                        throw new Exception("unrecognized instruction: " + s.Substring(0, 7));
+                }
+
+                string[] parts = s.Split(' ');
+                string[] point1 = parts[parts.Length - 3].Split(',');
+                string[] point2 = parts[parts.Length - 1].Split(',');
+
+                (int x, int y) firstPoint = (int.Parse(point1[0]), int.Parse(point1[1]));
+                (int x, int y) secondPoint = (int.Parse(point2[0]), int.Parse(point2[1])); ;
+
+                for (int i = firstPoint.x; i <= secondPoint.x; i++)
+                {
+                    for (int j = firstPoint.y; j <= secondPoint.y; j++)
+                    {
+                        grid[j * 1000 + i] += operation;
+                        if (grid[j * 1000 + i] < 0)
+                            grid[j * 1000 + i] = 0;
+                    }
+                }
+            }
+
+            return grid.Sum().ToString();
+        }
+    }
+}
