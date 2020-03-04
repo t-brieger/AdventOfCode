3cd16e94557fef6264f3ba3cae2d92c9a9d395c6
ichbineinnerd <xximtilixx@gmail.com>
2020-03-04 17:45:11 +0100

    2015/01

---
+++ AdventOfCode/Solutions/2015/Year2015Day01.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Solutions._2015
+{
+    class Year2015Day01 : Solution
+    {
+        public override string Part1(string input)
+        {
+            int floor = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                    floor++;
+                else if (input[i] == ')')
+                    floor--;
+            }
+
+            return floor.ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            int floor = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                    floor++;
+                else if (input[i] == ')')
+                    floor--;
+                if (floor < 0)
+                    return (i + 1).ToString();
+            }
+            return "-1";
+        }
+    }
+}
