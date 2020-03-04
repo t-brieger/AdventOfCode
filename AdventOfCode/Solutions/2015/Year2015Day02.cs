a35ff5e861a1a1e67a1a60483d6452c182bd4752
ichbineinnerd <xximtilixx@gmail.com>
2020-03-04 18:59:36 +0100

    2015/2-3

---
+++ AdventOfCode/Solutions/2015/Year2015Day02.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode.Solutions._2015
+{
+    class Year2015Day02 : Solution
+    {
+        public override string Part1(string input)
+        {
+            string[] lines = input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int totalArea = 0;
+
+            foreach (string line in lines)
+            {
+                int[] measurements = line.Split('x').Select(int.Parse).ToArray();
+                totalArea += 2 * (measurements[0] * measurements[1] + measurements[1] * measurements[2] + measurements[0] * measurements[2]);
+                totalArea += Math.Min(Math.Min(measurements[0] * measurements[1], measurements[1] * measurements[2]), measurements[0] * measurements[2]);
+            }
+
+            return totalArea.ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            string[] lines = input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int totalLength = 0;
+
+            foreach (string line in lines)
+            {
+                int[] measurements = line.Split('x').Select(int.Parse).ToArray();
+
+                totalLength += 2 * measurements.Sum() - 2 * measurements.Max();
+                totalLength += measurements[0] * measurements[1] * measurements[2];
+            }
+
+            return totalLength.ToString();
+        }
+    }
+}
---
+++ AdventOfCode/Solutions/2015/Year2015Day03.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Solutions._2015
+{
+    class Year2015Day03 : Solution
+    {
+        public override string Part1(string input)
+        {
+            int posX = 0;
+            int posY = 0;
+
+            HashSet<(int x, int y)> visited = new HashSet<(int x, int y)>();
+
+            visited.Add((0, 0));
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '<')
+                    posX--;
+                else if (input[i] == '>')
+                    posX++;
+                else if (input[i] == 'v')
+                    posY++;
+                else if (input[i] == '^')
+                    posY--;
+
+                if (!visited.Contains((posX, posY)))
+                    visited.Add((posX, posY));
+            }
+
+            return visited.Count.ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            int posX = 0;
+            int posY = 0;
+
+            int posX1 = 0;
+            int posY1 = 0;
+
+            bool isRobosTurn = false;
+
+            HashSet<(int x, int y)> visited = new HashSet<(int x, int y)>();
+
+            visited.Add((0, 0));
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '<')
+                    if (isRobosTurn)
+                        posX1--;
+                    else
+                        posX--;
+                else if (input[i] == '>')
+                    if (isRobosTurn)
+                        posX1++;
+                    else
+                        posX++;
+                else if (input[i] == 'v')
+                    if (isRobosTurn)
+                        posY1++;
+                    else
+                        posY++;
+                else if (input[i] == '^')
+                    if (isRobosTurn)
+                        posY1--;
+                    else
+                        posY--;
+
+                if (isRobosTurn)
+                {
+                    if (!visited.Contains((posX1, posY1)))
+                        visited.Add((posX1, posY1));
+                }
+                else
+                {
+                    if (!visited.Contains((posX, posY)))
+                        visited.Add((posX, posY));
+                }
+
+                isRobosTurn = !isRobosTurn;
+            }
+
+            return visited.Count.ToString();
+        }
+    }
+}
