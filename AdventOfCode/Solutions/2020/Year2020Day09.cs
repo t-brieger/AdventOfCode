f23c0497c92aeed06c7fff75ff18856601c5f8f8
ichbineinnerd <xximtilixx@gmail.com>
2020-12-13 22:28:45 +0100

    add 2020/09

---
+++ AdventOfCode/Solutions/2020/Year2020Day09.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Solutions
+{
+    public class Year2020Day09 : Solution
+    {
+        public override string Part1(string input)
+        {
+            long[] numbers = input.Split(new[] {'\n', '\r'}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse).ToArray();
+
+            for (int i = 25; i < numbers.Length; i++)
+            {
+                bool combinationFound = false;
+                for (int j = i - 25; j < i - 1; j++)
+                {
+                    for (int k = j + 1; k < i; k++)
+                    {
+                        if (numbers[j] + numbers[k] == numbers[i])
+                        {
+                            combinationFound = true;
+                            break;
+                        }
+                    }
+
+                    if (combinationFound)
+                        break;
+                }
+
+                if (!combinationFound)
+                {
+                    return numbers[i].ToString();
+                }
+            }
+
+            return null;
+        }
+
+        public override string Part2(string input)
+        {
+            
+            
+            //lol
+            long numberToSearch = long.Parse(Part1(input));
+
+            long[] numbers = input.Split(new[] {'\n', '\r'}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse).ToArray();
+
+            for (int start = 0; start < numbers.Length - 1; start++)
+            {
+                for (int end = start + 1; end < numbers.Length; end++)
+                {
+                    long[] slice = numbers.Skip(start).Take(end - start + 1).ToArray();
+
+                    if (slice.Sum() == numberToSearch)
+                        return (slice.Min() + slice.Max()).ToString();
+                }
+            }
+
+            return null;
+        }
+    }
+}
