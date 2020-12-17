5334ed5cc634990cb7c015cca16deb654960d1f7
ichbineinnerd <xximtilixx@gmail.com>
2020-12-17 22:17:02 +0100

    add 2015/08

---
+++ AdventOfCode/Solutions/2015/Year2015Day08.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Solutions
+{
+    public class Year2015Day08 : Solution
+    {
+        public override string Part1(string input)
+        {
+            string[] strings = input.Split('\n', StringSplitOptions.RemoveEmptyEntries)
+                .Select(s => s.Substring(1, s.Length - 2)).ToArray();
+            int actualTextLength = 0;
+
+            foreach (string s in strings)
+            {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    actualTextLength++;
+                    if (s[i] == '\\')
+                    {
+                        if (s[i + 1] == '\\' || s[i + 1] == '"')
+                            i++;
+                        else
+                            i += 3;
+                    }
+                }
+            }
+
+            return (strings.Sum(s => s.Length + 2) - actualTextLength).ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            string[] strings = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+            string[] encodedStrings = strings.Select(s =>
+                '"' + s.Replace("\"", "DOUBLEQUOTE").Replace("\\", "BACKSLASH").Replace("DOUBLEQUOTE", "\\\"")
+                    .Replace("BACKSLASH", "\\\\") + '"').ToArray();
+
+            return (encodedStrings.Sum(s => s.Length) - strings.Sum(s => s.Length)).ToString();
+        }
+    }
+}
