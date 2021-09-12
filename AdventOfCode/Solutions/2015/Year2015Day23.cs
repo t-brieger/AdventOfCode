bae2293b6eba6756ad760a5ff008aadbad66bb73
Nerdyyy <xximtilixx@gmail.com>
2021-09-12 18:45:34 +0200

    2015/23 was... surprisingly easy?

---
+++ AdventOfCode/Solutions/2015/Year2015Day23.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+
+namespace AdventOfCode.Solutions
+{
+    public class Year2015Day23 : Solution
+    {
+        public override string Part1(string input)
+        {
+            (string, string)[] instructions = input.Trim().Split('\n').Select(l => (l[..3], l[4..])).ToArray();
+            int ip = 0;
+
+            Dictionary<char, uint> registers = new()
+            {
+                ['a'] = 0,
+                ['b'] = 0
+            };
+
+            while (ip < instructions.Length && ip >= 0)
+            {
+                (string op, string args) = instructions[ip];
+                string[] argv = args.Split(", ");
+                switch (op)
+                {
+                    case "hlf":
+                        registers[args[0]] /= 2;
+                        break;
+                    case "tpl":
+                        registers[args[0]] *= 3;
+                        break;
+                    case "inc":
+                        registers[args[0]]++;
+                        break;
+                    case "jmp":
+                        ip += int.Parse(args) - 1;
+                        break;
+                    case "jie":
+                        if (registers[argv[0][0]] % 2 == 0)
+                            ip += int.Parse(argv[1]) - 1;
+                        break;
+                    case "jio":
+                        if (registers[argv[0][0]] == 1)
+                            ip += int.Parse(argv[1]) - 1;
+                        break;
+                }
+
+                ip++;
+            }
+
+            return registers['b'].ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            (string, string)[] instructions = input.Trim().Split('\n').Select(l => (l[..3], l[4..])).ToArray();
+            int ip = 0;
+
+            Dictionary<char, uint> registers = new()
+            {
+                ['a'] = 1,
+                ['b'] = 0
+            };
+
+            while (ip < instructions.Length && ip >= 0)
+            {
+                (string op, string args) = instructions[ip];
+                string[] argv = args.Split(", ");
+                switch (op)
+                {
+                    case "hlf":
+                        registers[args[0]] /= 2;
+                        break;
+                    case "tpl":
+                        registers[args[0]] *= 3;
+                        break;
+                    case "inc":
+                        registers[args[0]]++;
+                        break;
+                    case "jmp":
+                        ip += int.Parse(args) - 1;
+                        break;
+                    case "jie":
+                        if (registers[argv[0][0]] % 2 == 0)
+                            ip += int.Parse(argv[1]) - 1;
+                        break;
+                    case "jio":
+                        if (registers[argv[0][0]] == 1)
+                            ip += int.Parse(argv[1]) - 1;
+                        break;
+                }
+
+                ip++;
+            }
+
+            return registers['b'].ToString();        }
+    }
+}
