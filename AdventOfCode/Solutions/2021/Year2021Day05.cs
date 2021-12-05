d350905cc08c1a57302f158edfa02c27086ecf16
A Name <xximtilixx@gmail.com>
2021-12-05 06:24:21 +0100

    [2021/05] global #221 and #727

---
+++ AdventOfCode/Solutions/2021/Year2021Day05.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Solutions
+{
+    public class Year2021Day05 : Solution
+    {
+        public override string Part1(string input)
+        {
+            input = input.Replace(" -> ", ",");
+
+            int[][] lines = input.Split('\n').Select(l => l.Split(",").Select(int.Parse).ToArray())
+                .ToArray();
+
+            HashSet<(int, int)> seen = new HashSet<(int, int)>();
+            HashSet<(int, int)> seenTwice = new HashSet<(int, int)>();
+
+            foreach (int[] line in lines)
+            {
+                if (line[0] != line[2] && line[1] != line[3])
+                    continue;
+
+                for (int x = Math.Min(line[0], line[2]); x <= Math.Max(line[0], line[2]); x++)
+                {
+                    for (int y = Math.Min(line[1], line[3]); y <= Math.Max(line[1], line[3]); y++)
+                    {
+                        if (seen.Contains((x, y)))
+                            seenTwice.Add((x, y));
+                        else
+                            seen.Add((x, y));
+                    }
+                }
+            }
+
+            return seenTwice.Count.ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            input = input.Replace(" -> ", ",");
+
+            int[][] lines = input.Split('\n').Select(l => l.Split(",").Select(int.Parse).ToArray())
+                .ToArray();
+
+            HashSet<(int, int)> seen = new HashSet<(int, int)>();
+            HashSet<(int, int)> seenTwice = new HashSet<(int, int)>();
+
+            foreach (int[] line in lines)
+            {
+                if (line[0] != line[2] && line[1] != line[3])
+                {
+                    int x = line[0];
+                    int y = line[1];
+
+                    for (int steps = 0; steps <= Math.Abs(line[0] - line[2]); steps++)
+                    {
+                        if (seen.Contains((x, y)))
+                            seenTwice.Add((x, y));
+                        else
+                            seen.Add((x, y));
+
+                        if (line[0] > line[2])
+                            x--;
+                        else
+                            x++;
+
+                        if (line[1] > line[3])
+                            y--;
+                        else
+                            y++;
+                    }
+
+                    continue;
+                }
+
+                for (int x = Math.Min(line[0], line[2]); x <= Math.Max(line[0], line[2]); x++)
+                {
+                    for (int y = Math.Min(line[1], line[3]); y <= Math.Max(line[1], line[3]); y++)
+                    {
+                        if (seen.Contains((x, y)))
+                            seenTwice.Add((x, y));
+                        else
+                            seen.Add((x, y));
+                    }
+                }
+            }
+
+            return seenTwice.Count.ToString();
+        }
+    }
+}
