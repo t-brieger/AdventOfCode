4aef207801aacfc6d9dd752e278156a31ebab1ea
A Name <xximtilixx@gmail.com>
2021-12-07 15:56:51 +0100

    [2021/07]

---
+++ AdventOfCode/Solutions/2021/Year2021Day07.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode.Solutions
+{
+    public class Year2021Day07 : Solution
+    {
+        public override string Part1(string input)
+        {
+            int[] positions = input.Split(',').Select(int.Parse).ToArray();
+            int leftMost = positions.Min();
+            int rightMost = positions.Max();
+
+            int minFuel = int.MaxValue;
+            for (int i = leftMost; i <= rightMost; i++)
+            {
+                minFuel = Math.Min(positions.Sum(e => Math.Abs(e - i)), minFuel);
+            }
+
+            return minFuel.ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            int[] positions = input.Split(',').Select(int.Parse).ToArray();
+            int leftMost = positions.Min();
+            int rightMost = positions.Max();
+
+            int minFuel = int.MaxValue;
+            for (int i = leftMost; i <= rightMost; i++)
+            {
+                minFuel = Math.Min(positions.Sum(e =>
+                {
+                    int stepsToMove = Math.Abs(e - i);
+                    return stepsToMove * (stepsToMove + 1) / 2;
+                }), minFuel);
+            }
+
+            return minFuel.ToString();
+        }
+    }
+}
