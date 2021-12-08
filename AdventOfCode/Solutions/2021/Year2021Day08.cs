f8ffbbb9dc2aecb83fcb169bd43ce4cc13a3a033
A Name <xximtilixx@gmail.com>
2021-12-08 15:11:15 +0100

    [2021/08]

---
+++ AdventOfCode/Solutions/2021/Year2021Day08.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+
+namespace AdventOfCode.Solutions
+{
+    public class Year2021Day08 : Solution
+    {
+        public override string Part1(string input)
+        {
+            return input.Split('\n').Select(l => l.Split(" | ")[1])
+                .Select(output => output.Split(' ').Count(x => x.Length is 2 or 4 or 3 or 7)).Sum().ToString();
+        }
+
+        public override string Part2(string input)
+        {
+            string[] displays = input.Split('\n');
+
+            int outputSum = 0;
+
+            foreach (string display in displays)
+            {
+                (string[] test, string[] data) = display.Split(" | ").Select(x => x.Split(' ')).ToArray();
+
+                Dictionary<int, string> numbermap = new Dictionary<int, string>();
+                for (int i = 0; i <= 9; i++)
+                    numbermap[i] = "";
+
+                // might have to iterate multiple times
+                bool changes = true;
+                while (changes)
+                {
+                    changes = false;
+                    Dictionary<int, string> oldNumMap = new Dictionary<int, string>(numbermap);
+                    foreach (string rawcode in test)
+                    {
+                        string code = new string(rawcode.ToCharArray().OrderByDescending(c => (int) c).ToArray());
+                        switch (code.Length)
+                        {
+                            case 2:
+                                numbermap[1] = code;
+                                break;
+                            case 3:
+                                numbermap[7] = code;
+                                break;
+                            case 4:
+                                numbermap[4] = code;
+                                break;
+                            case 7:
+                                numbermap[8] = code;
+                                break;
+                            case 5:
+                                if (code.Count(c => numbermap[1].Contains(c)) == 2)
+                                    numbermap[3] = code;
+                                else if (code.Count(c => numbermap[4].Contains(c)) == 2)
+                                    numbermap[2] = code;
+                                else if (code.Count(c => numbermap[4].Contains(c)) == 3)
+                                    numbermap[5] = code;
+                                break;
+                            case 6:
+                                if (code.Count(c => numbermap[4].Contains(c)) == 4)
+                                    numbermap[9] = code;
+                                else if (code.Count(c => numbermap[5].Contains(c)) == 5)
+                                    numbermap[6] = code;
+                                else if (code.Count(c => numbermap[5].Contains(c)) == 4)
+                                    numbermap[0] = code;
+                                break;
+                        }
+                    }
+
+                    for (int i = 0; i <= 9; i++)
+                    {
+                        if (numbermap[i] != oldNumMap[i])
+                            changes = true;
+                    }
+                }
+
+                int tmpSum = 0;
+                
+                foreach (string rawcode in data)
+                {
+                    string code = new string(rawcode.ToCharArray().OrderByDescending(c => (int) c).ToArray());
+                    
+                    tmpSum *= 10;
+                    tmpSum += numbermap.First(kvp => kvp.Value == code).Key;
+                }
+
+                outputSum += tmpSum;
+            }
+
+            return outputSum.ToString();
+        }
+    }
+}
