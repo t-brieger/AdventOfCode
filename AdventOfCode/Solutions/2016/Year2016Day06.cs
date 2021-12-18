744d05e5f73379ae0b915affc99566a586a25205
A Name <xximtilixx@gmail.com>
2021-12-18 23:22:51 +0100

    [2016/06,07]

---
+++ AdventOfCode/Solutions/2016/Year2016Day06.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Solutions;
+
+public class Year2016Day06 : Solution
+{
+    public override string Part1(string input)
+    {
+        string[] lines = input.Split('\n');
+
+        string s = "";
+        
+        for (int i = 0; i < lines[0].Length; i++)
+        {
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                counts[c] = lines.Count(x => x[i] == c);
+            }
+
+            s += counts.MaxBy(kvp => kvp.Value).Key;
+        }
+
+        return s;
+    }
+
+    public override string Part2(string input)
+    {
+        string[] lines = input.Split('\n');
+
+        string s = "";
+        
+        for (int i = 0; i < lines[0].Length; i++)
+        {
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+            for (char c = 'a'; c <= 'z'; c++)
+            {
+                counts[c] = lines.Count(x => x[i] == c);
+            }
+
+            s += counts.MinBy(kvp => kvp.Value).Key;
+        }
+
+        return s;
+    }
+}
---
+++ AdventOfCode/Solutions/2016/Year2016Day07.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Solutions;
+
+public class Year2016Day07 : Solution
+{
+    private static bool IsValidIpPart1(string[] ip)
+    {
+        bool symmetricOutsideOfBrackets = false;
+        for (int i = 0; i < ip.Length; i++)
+        {
+            bool foundSymmetric = false;
+            for (int j = 0; j <= ip[i].Length - 4; j++)
+            {
+                if (ip[i][j] == ip[i][j + 3] && ip[i][j + 1] == ip[i][j + 2] && ip[i][j] != ip[i][j + 1])
+                {
+                    foundSymmetric = true;
+                    break;
+                }
+            }
+
+            if (foundSymmetric && i % 2 == 1)
+                return false;
+            if (foundSymmetric && i % 2 == 0)
+                symmetricOutsideOfBrackets = true;
+        }
+
+        return symmetricOutsideOfBrackets;
+    }
+
+    private static bool IsValidIpPart2(string[] ip)
+    {
+        HashSet<(char, char)> aba = new HashSet<(char, char)>();
+        for (int i = 0; i < ip.Length; i += 2)
+        {
+            for (int j = 0; j <= ip[i].Length - 3; j++)
+            {
+                if (ip[i][j] == ip[i][j + 2] && ip[i][j + 1] != ip[i][j])
+                    aba.Add((ip[i][j], ip[i][j + 1]));
+            }
+        }
+        
+        for (int i = 1; i < ip.Length; i += 2)
+        {
+            foreach ((char a, char b) in aba)
+                if (ip[i].Contains("" + b + a + b))
+                    return true;
+        }
+
+        return false;
+    }
+
+    public override string Part1(string input)
+    {
+        string[][] ips = input.Split('\n').Select(line => line.Split(new[] {']', '['})).ToArray();
+
+        return ips.Count(IsValidIpPart1).ToString();
+    }
+
+    public override string Part2(string input)
+    {
+        string[][] ips = input.Split('\n').Select(line => line.Split(new[] {']', '['})).ToArray();
+
+        return ips.Count(IsValidIpPart2).ToString();
+    }
+}
