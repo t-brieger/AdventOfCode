39bb441fd5399bda569ee437bb430630a564a9d0
T. Brieger <tilman.brieger@gmx.de>
2022-01-01 23:17:10 +0100

    [2021/23]

--- AdventOfCode/Solutions/2021/Year2021Day23.cs
+++ AdventOfCode/Solutions/2021/Year2021Day23.cs
@@ -1,138 +1,258 @@
-using System.Formats.Asn1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode.Solutions;
 
 public class Year2021Day23 : Solution
 {
-    private static uint Encode(int[] state)
+    private enum FieldState
     {
-        for (int i = 0; i < 8; i += 2)
-        {
-            if (state[i] < state[i + 1])
-                continue;
-            int _ = state[i];
-            state[i] = Math.Min(state[i], state[i + 1]);
-            state[i + 1] = Math.Max(_, state[i + 1]);
-        }
-        List<int> l = new(19);
-        for (int i = 0; i < 19; i++)
-            l.Add(-1);
-        for (int i = 0; i < 8; i++)
-            l[state[i]] = i;
-
-        uint result = 0;
-        for (int i = 0; i < 8; i++)
-        {
-            result *= 18 - (uint)i + 1;
-            result += (uint)l.IndexOf(i);
-            l.Remove(i);
-        }
-
-        return result;
+        A,
+        B,
+        C,
+        D,
+        EMPTY
     }
 
-    private static int[] Decode(uint state)
+    private class Burrow
     {
-        List<int> result = new(19);
-        for (int i = 0; i < 11; i++)
-            result.Add(-1);
-        for (int i = 7; i >= 0; i--)
+        public FieldState[] hallway;
+        public Stack<FieldState>[] sides;
+
+        private Burrow Clone()
         {
-            result.Insert((int)(state % (18 - i + 1)), i);
-            state = (uint)(state / (18 - i + 1));
+            Burrow n = new Burrow();
+            n.hallway = new FieldState[11];
+            for (int i = 0; i < 11; i++)
+                n.hallway[i] = this.hallway[i];
+
+            n.sides = new Stack<FieldState>[4];
+            for (int i = 0; i < 4; i++)
+            {
+                List<FieldState> l = this.sides[i].ToList();
+                l.Reverse();
+                n.sides[i] = new Stack<FieldState>(l);
+            }
+
+            return n;
         }
 
-        int[] output = new int[8];
-        for (int i = 0; i < 8; i++)
-            output[i] = result.IndexOf(i);
 
-        return output;
+        private static int Cost(FieldState fs)
+        {
+            // ReSharper disable once SwitchExpressionHandlesSomeKnownEnumValuesWithExceptionInDefault
+            return fs switch
+            {
+                FieldState.A => 1,
+                FieldState.B => 10,
+                FieldState.C => 100,
+                FieldState.D => 1000,
+                _ => throw new ArgumentOutOfRangeException(nameof(fs), fs, null)
+            };
+        }
+
+        public IEnumerable<(Burrow newstate, int cost)> GetNextStates(bool p2 = false)
+        {
+            // move out of side corridors
+            for (int i = 0; i < 4; i++)
+            {
+                if (sides[i].Count == 0)
+                    continue;
+                if (sides[i].All(e => e == (FieldState)i))
+                    continue;
+                // empty spaces to the left
+                for (int j = 2 * i + 2; j >= 0; j--)
+                {
+                    if (this.hallway[j] != FieldState.EMPTY)
+                        break;
+                    if (j is 2 or 4 or 6 or 8)
+                        continue;
+                    Burrow b = Clone();
+                    FieldState x = b.sides[i].Pop();
+                    b.hallway[j] = x;
+                    yield return (b, Cost(x) * ((p2 ? 4 : 2) - b.sides[i].Count + Math.Abs(2 * i + 2 - j)));
+                }
+
+                // ...to the right
+                for (int j = 2 * i + 2; j < 11; j++)
+                {
+                    if (this.hallway[j] != FieldState.EMPTY)
+                        break;
+                    if (j is 2 or 4 or 6 or 8)
+                        continue;
+                    Burrow b = Clone();
+                    FieldState x = b.sides[i].Pop();
+                    b.hallway[j] = x;
+                    yield return (b, Cost(x) * ((p2 ? 4 : 2) - b.sides[i].Count + Math.Abs(2 * i + 2 - j)));
+                }
+            }
+
+            // move into rooms
+            for (int i = 0; i < 11; i++)
+            {
+                if (this.hallway[i] == FieldState.EMPTY)
+                    continue;
+                int sideIndex = (int)this.hallway[i];
+                int goalHallwayPos = sideIndex * 2 + 2;
+                bool foundConflict = false;
+                for (int j = Math.Min(i, goalHallwayPos); j <= Math.Max(i, goalHallwayPos); j++)
+                {
+                    if (j == i)
+                        continue;
+                    if (this.hallway[j] != FieldState.EMPTY)
+                    {
+                        foundConflict = true;
+                        break;
+                    }
+                }
+
+                if (foundConflict) continue;
+
+                // no obstacles to moving into our matching room - check if it is even possible by the rules
+                if (this.sides[sideIndex].All(e => e == this.hallway[i]))
+                {
+                    Burrow b = this.Clone();
+                    b.hallway[i] = FieldState.EMPTY;
+                    b.sides[sideIndex].Push(this.hallway[i]);
+
+                    yield return (b,
+                        Cost(this.hallway[i]) * (Math.Abs(i - (sideIndex * 2 + 2)) + (p2 ? 4 : 2) -
+                                                 this.sides[sideIndex].Count));
+                }
+            }
+        }
+
+        public long Hash()
+        {
+            // hash -> 4 * <side> (12 bits) <hallway> (21 bits)
+            //      -> 33 bits (almost an int, oh well)
+            // side -> <stack count (2 bits)> <top element matches? (1 bit)> 
+            // hallway -> 3 bit enum value for every field not directly above a side corridor (= 21)
+            long val = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                val <<= 2;
+                val |= (uint)this.sides[i].Count;
+                val <<= 1;
+                val |= (uint)(this.sides[i].Count != 0 && this.sides[i].Peek() == (FieldState)i ? 1 : 0);
+            }
+
+            for (int i = 0; i < 11; i++)
+            {
+                if (i is 2 or 4 or 6 or 8)
+                    continue;
+                val <<= 3;
+                val |= (long)this.hallway[i];
+            }
+
+            return val;
+        } 
     }
 
     public override string Part1(string input)
     {
-        PriorityQueue<(uint state, int weight), int> queue = new();
-        input = input.Replace(" ", "").Replace("\n", "").Replace("#", "");
-        queue.Enqueue((Encode(new[]
-            {
-                input.IndexOf('A'), input.LastIndexOf('A'),
-                input.IndexOf('B'), input.LastIndexOf('B'),
-                input.IndexOf('C'), input.LastIndexOf('C'),
-                input.IndexOf('D'), input.LastIndexOf('D')
-            }), 0), 0);
+        Burrow b = new()
+        {
+            sides = new Stack<FieldState>[4],
+            hallway = new FieldState[11]
+        };
+        for (int i = 0; i < 11; i++)
+            b.hallway[i] = FieldState.EMPTY;
+        for (int i = 0; i < 4; i++)
+            b.sides[i] = new Stack<FieldState>();
+        b.sides[0].Push((FieldState)(input[45] - 'A'));
+        b.sides[0].Push((FieldState)(input[31] - 'A'));
 
-        HashSet<uint> seenStates = new HashSet<uint>();
+        b.sides[1].Push((FieldState)(input[47] - 'A'));
+        b.sides[1].Push((FieldState)(input[33] - 'A'));
+
+        b.sides[2].Push((FieldState)(input[49] - 'A'));
+        b.sides[2].Push((FieldState)(input[35] - 'A'));
+
+        b.sides[3].Push((FieldState)(input[51] - 'A'));
+        b.sides[3].Push((FieldState)(input[37] - 'A'));
+
+        PriorityQueue<(Burrow b, int weight), int> queue = new();
+        queue.Enqueue((b, 0), 0);
+        HashSet<long> seenStates = new HashSet<long>();
 
         while (true)
         {
-            (uint state, int weight) = queue.Dequeue();
-            if (seenStates.Contains(state))
+            (Burrow state, int weight) = queue.Dequeue();
+            if (seenStates.Contains(state.Hash()))
                 continue;
-            seenStates.Add(state);
-            if (seenStates.Count % 1000000 == 0)
-                Console.WriteLine(seenStates.Count / 1000000);
-            int[] decodedState = Decode(state);
+            seenStates.Add(state.Hash());
 
-            for (int i = 0; i < 8; i++)
+            if (state.hallway.All(x => x == FieldState.EMPTY) && state.sides.All(s => s.Count == 2))
             {
-                if (decodedState[i] != 11 + i / 2 && decodedState[i] != 15 + i / 2)
-                    break;
-                if (i == 7)
+                bool err = false;
+                for (int i = 0; i < 4; i++)
+                    if (state.sides[i].Any(e => e != (FieldState)i))
+                        err = true;
+                if (!err)
                     return weight.ToString();
             }
-            
-            for (int i = 0; i < 8; i++)
-            {
-                int[] decodedStateCopy = new int[8];
-                decodedState.CopyTo(decodedStateCopy, 0);
-                int moveWeight = i / 2 == 0 ? 1 : i / 2 == 1 ? 10 : i / 2 == 2 ? 100 : 1000;
-                // move into matching hole
-                if (decodedState[i] == i / 2 * 2 + 2 && !decodedState.Contains(11 + i / 2))
-                {
-                    decodedStateCopy[i] = 11 + i / 2;
-                    queue.Enqueue((Encode(decodedStateCopy), weight + moveWeight), weight + moveWeight);
-                    decodedState.CopyTo(decodedStateCopy, 0);
-                }
-                // move right in top row
-                if (decodedState[i] is >= 0 and < 10 && !decodedState.Contains(decodedState[i] + 1))
-                {
-                    decodedStateCopy[i]++;
-                    queue.Enqueue((Encode(decodedStateCopy), weight + moveWeight), weight + moveWeight);
-                    decodedState.CopyTo(decodedStateCopy, 0);
-                }
-                // move left in top row
-                if (decodedState[i] is > 0 and <= 10 && !decodedState.Contains(decodedState[i] - 1))
-                {
-                    decodedStateCopy[i]--;
-                    queue.Enqueue((Encode(decodedStateCopy), weight + moveWeight), weight + moveWeight);
-                    decodedState.CopyTo(decodedStateCopy, 0);
-                }
-                // move down in hole
-                if (decodedState[i] is > 10 and <= 14 && !decodedState.Contains(decodedState[i] + 4))
-                {
-                    decodedStateCopy[i] += 4;
-                    queue.Enqueue((Encode(decodedStateCopy), weight + moveWeight), weight + moveWeight);
-                    decodedState.CopyTo(decodedStateCopy, 0);
-                }
-                // move up in hole (from bottom to middle)
-                if (decodedState[i] > 14 && !decodedState.Contains(decodedState[i] - 4))
-                {
-                    decodedStateCopy[i] -= 4;
-                    queue.Enqueue((Encode(decodedStateCopy), weight + moveWeight), weight + moveWeight);
-                    decodedState.CopyTo(decodedStateCopy, 0);
-                }
-                // move up in hole (from middle to top)
-                if (decodedState[i] is > 10 and <= 14 && !decodedState.Contains((decodedState[i] - 11) * 2 + 2))
-                {
-                    decodedStateCopy[i] = (decodedState[i] - 11) * 2 + 2;
-                    queue.Enqueue((Encode(decodedStateCopy), weight + moveWeight), weight + moveWeight);
-                }
-            }
+
+            foreach ((Burrow next, int cost) in state.GetNextStates())
+                queue.Enqueue((next, weight + cost), weight + cost);
         }
     }
 
     public override string Part2(string input)
     {
-        throw new NotImplementedException();
+        Burrow b = new()
+        {
+            sides = new Stack<FieldState>[4],
+            hallway = new FieldState[11]
+        };
+        for (int i = 0; i < 11; i++)
+            b.hallway[i] = FieldState.EMPTY;
+        for (int i = 0; i < 4; i++)
+            b.sides[i] = new Stack<FieldState>();
+        b.sides[0].Push((FieldState)(input[45] - 'A'));
+        b.sides[0].Push(FieldState.D);
+        b.sides[0].Push(FieldState.D);
+        b.sides[0].Push((FieldState)(input[31] - 'A'));
+
+        b.sides[1].Push((FieldState)(input[47] - 'A'));
+        b.sides[1].Push(FieldState.B);
+        b.sides[1].Push(FieldState.C);
+        b.sides[1].Push((FieldState)(input[33] - 'A'));
+
+        b.sides[2].Push((FieldState)(input[49] - 'A'));
+        b.sides[2].Push(FieldState.A);
+        b.sides[2].Push(FieldState.B);
+        b.sides[2].Push((FieldState)(input[35] - 'A'));
+
+        b.sides[3].Push((FieldState)(input[51] - 'A'));
+        b.sides[3].Push(FieldState.C);
+        b.sides[3].Push(FieldState.A);
+        b.sides[3].Push((FieldState)(input[37] - 'A'));
+
+        PriorityQueue<(Burrow b, int weight), int> queue = new();
+        queue.Enqueue((b, 0), 0);
+        HashSet<long> seenStates = new HashSet<long>();
+
+        while (true)
+        {
+            (Burrow state, int weight) = queue.Dequeue();
+            if (seenStates.Contains(state.Hash()))
+                continue;
+            seenStates.Add(state.Hash());
+
+            if (state.hallway.All(x => x == FieldState.EMPTY) && state.sides.All(s => s.Count == 4))
+            {
+                bool err = false;
+                for (int i = 0; i < 4; i++)
+                    if (state.sides[i].Any(e => e != (FieldState)i))
+                        err = true;
+                if (!err)
+                    return weight.ToString();
+            }
+
+            foreach ((Burrow next, int cost) in state.GetNextStates(true))
+                queue.Enqueue((next, weight + cost), weight + cost);
+        }
     }
 }
