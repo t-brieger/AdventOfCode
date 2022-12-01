784cdd3d9ebccc8b0a7eda8a4178b9c2138ce3dd
T Brieger <tilman.brieger@gmx.de>
2022-12-01 12:31:30 +0100

    2022 part 1

---
+++ AdventOfCode/Solutions/2022/Year2022Day01.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace AdventOfCode.Solutions;
+
+public class Year2022Day01 : Solution
+{
+    public override string Part1(string input)
+    {
+        return input.Split("\n\n").Select(s => s.Split('\n')).Select(a => a.Select(int.Parse).Sum()).OrderByDescending(i => i)
+            .First().ToString();
+    }
+
+    public override string Part2(string input)
+    {
+        return input.Split("\n\n").Select(s => s.Split('\n')).Select(a => a.Select(int.Parse).Sum()).OrderByDescending(i => i)
+            .Take(3).Sum().ToString();
+    }
+}
