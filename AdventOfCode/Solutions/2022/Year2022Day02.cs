3227a8a523cc3bcbd992e975426e05108ce8a56b
T Brieger <tilman.brieger@gmx.de>
2022-12-02 06:19:18 +0100

    2022 day 2 GLOBAL LEADERBOARD WHOOO

---
+++ AdventOfCode/Solutions/2022/Year2022Day02.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode.Solutions;
+
+public class Year2022Day02 : Solution
+{
+    public override string Part1(string input)
+    {
+        int score = 0;
+        foreach (string line in input.Split('\n'))
+        {
+            if (line == "A Y" || line == "B Z" || line == "C X")
+                score += 6;
+            if (line == "A X" || line == "B Y" || line == "C Z")
+                score += 3;
+
+            if (line.Contains("X"))
+                score += 1;
+            if (line.Contains("Y"))
+                score += 2;
+            if (line.Contains("Z"))
+                score += 3;
+        }
+        return score.ToString();
+    }
+
+    public override string Part2(string input)
+    {
+        int score = 0;
+        Dictionary<char, char> a = new Dictionary<char, char>();
+        foreach (string line in input.Split('\n'))
+        {
+            if (line.Contains("X"))
+            {
+                score += line[0] == 'A' ? 3 : line[0] == 'B' ? 1 : 2;
+            }
+            if (line.Contains("Y"))
+            {
+                score += line[0] == 'A' ? 4 : line[0] == 'B' ? 5 : 6;
+            }
+            if (line.Contains("Z"))
+            {
+                score += line[0] == 'A' ? 8 : line[0] == 'B' ? 9 : 7;
+            }
+        }
+        return score.ToString();
+    }
+}
