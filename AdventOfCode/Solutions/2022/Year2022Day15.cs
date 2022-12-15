97925707c49305bebc9bc19221863fc5a6d8a84f
T Brieger <tilman.brieger@gmx.de>
2022-12-15 18:59:06 +0100

    do the smart thing (day15)

--- AdventOfCode/Solutions/2022/Year2022Day15.cs
+++ AdventOfCode/Solutions/2022/Year2022Day15.cs
@@ -86,90 +86,56 @@ public class Year2022Day15 : Solution
 
     public override string Part2(string input)
     {
-        List<(int sensX, int sensY, int beacX, int beacY)> sensors = new();
+        List<(int sensX, int sensY, int radius)> sensors = new();
 
         foreach (string sensorReading in input.Split('\n'))
         {
             string[] parts = sensorReading.Replace(",", "").Replace(":", "").Split(' ');
 
-            sensors.Add((int.Parse(parts[2][2..]), int.Parse(parts[3][2..]), int.Parse(parts[8][2..]),
-                int.Parse(parts[9][2..])));
+            (int, int, int, int) tmpTuple = (int.Parse(parts[2][2..]), int.Parse(parts[3][2..]),
+                int.Parse(parts[8][2..]),
+                int.Parse(parts[9][2..]));
+
+            sensors.Add((tmpTuple.Item1, tmpTuple.Item2, Math.Abs(tmpTuple.Item1 - tmpTuple.Item3) + Math.Abs(tmpTuple.Item2 - tmpTuple.Item4)));
         }
 
-        RescaleBar(4_000_001 + sensors.Count);
-        
-        List<(int, int)>[] emptyRanges = new List<(int, int)>[4_000_001];
+        RescaleBar(4 * sensors.Count * sensors.Count);
 
-        foreach ((int sensX, int sensY, int beacX, int beacY) in sensors)
+        List<int> linesLeft = new(), linesRight = new();
+
+
+        foreach ((int x, int y, int radius) in sensors)
         {
-            int radius = Math.Abs(sensX - beacX) + Math.Abs(sensY - beacY);
-            for (int i = Math.Max(sensY - radius, 0); i <= Math.Min(sensY + radius, 4_000_000); i++)
-            {
-                int range = radius - Math.Abs(sensY - i);
-
-                if (range < 0)
-                    continue;
-
-                if (emptyRanges[i] == null)
-                    emptyRanges[i] = new List<(int, int)>();
-
-                emptyRanges[i].Add((sensX - range, sensX + range));
-            }
-
-            IncreaseBar();
+            linesRight.Add(y - x + radius + 1);
+            linesRight.Add(y - x - radius - 1);
+            
+            linesLeft.Add(x + y + radius + 1);
+            linesLeft.Add(x + y - radius - 1);
+            
         }
 
-        for (int i = 0; i < emptyRanges.Length; i++)
+        foreach (int l in linesLeft)
         {
-            bool changed = true;
-            while (changed)
+            foreach (int r in linesRight)
             {
-                emptyRanges[i] = emptyRanges[i].Distinct().ToList();
-                changed = false;
-                foreach ((int start, int end) in emptyRanges[i])
+                //both have same value: intersection = (0, val)
+                //r = l + 2: intersection = (1, l + r / 2)
+                (int x, int y) intersection = ((l - r) / 2, (l + r) / 2);
+
+                if (intersection.x is >= 0 and <= 4_000_000 && intersection.y is >= 0 and <= 4_000_000)
                 {
-                    if (emptyRanges[i].Any(r => r != (start, end) && r.Item1 <= start && r.Item2 >= end))
+                    if (sensors.All(s =>
+                            Math.Abs(s.sensX - intersection.x) + Math.Abs(s.sensY - intersection.y) > s.radius))
                     {
-                        emptyRanges[i].Remove((start, end));
-                        changed = true;
-                        break;
-                    }
-
-                    IEnumerable<(int, int)> otherPossibilities;
-                    otherPossibilities = emptyRanges[i].Where(r =>
-                        r != (start, end) && r.Item1 <= start && r.Item2 <= end && r.Item2 >= start);
-                    if (otherPossibilities.Any())
-                    {
-                        (int, int) other = otherPossibilities.First();
-                        emptyRanges[i] = emptyRanges[i].Select(e => e == other ? (other.Item1, end) : e).ToList();
-                        emptyRanges[i].Remove((start, end));
-                        changed = true;
-                        break;
-                    }
-
-                    otherPossibilities = emptyRanges[i].Where(r =>
-                        r != (start, end) && r.Item1 >= start && r.Item1 <= end && r.Item2 >= end);
-                    if (otherPossibilities.Any())
-                    {
-                        (int, int) other = otherPossibilities.First();
-                        emptyRanges[i] = emptyRanges[i].Select(e => e == other ? (start, other.Item2) : e).ToList();
-                        emptyRanges[i].Remove((start, end));
-                        changed = true;
-                        break;
+                        SetBar(GetBarScale());
+                        return (intersection.x * 4_000_000L + intersection.y).ToString();
                     }
                 }
-            }
 
-            IncreaseBar();
-
-            if (emptyRanges[i]
-                .Any(range => range.Item1 is >= 0 and <= 4_000_000 || range.Item2 is >= 0 and <= 4_000_000))
-            {
-                SetBar(GetBarScale());
-                return (4_000_000L * (emptyRanges[i][0].Item2 + 1) + i).ToString();
+                IncreaseBar();
             }
         }
-
+        
         return null;
     }
 }
