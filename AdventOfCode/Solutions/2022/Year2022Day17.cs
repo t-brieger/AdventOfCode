f26c7f9ad5f502f5dc6088ec79008fb2c9289d7e
T. Brieger <tilman.brieger@gmx.de>
2022-12-17 18:32:41 +0100

    small day 17 refactor

--- AdventOfCode/Solutions/2022/Year2022Day17.cs
+++ AdventOfCode/Solutions/2022/Year2022Day17.cs
@@ -6,9 +6,9 @@ namespace AdventOfCode.Solutions;
 
 public class Year2022Day17 : Solution
 {
-    private static List<(long, long)> GetNextRock(long index, long yPos)
+    private static HashSet<(long, long)> GetNextRock(long index, long yPos)
     {
-        List<(long, long)> ourRock = new List<(long, long)>();
+        HashSet<(long, long)> ourRock = new HashSet<(long, long)>();
         switch (index % 5)
         {
             case 0:
@@ -48,60 +48,83 @@ public class Year2022Day17 : Solution
         return ourRock;
     }
 
+    private static int  DoSimulationIteration(HashSet<(long, long)> rocks, long iteration, string jets, int index)
+    {
+        long yPos = rocks.Max(r => r.Item2) + 4;
+        HashSet<(long, long)> ourRock = GetNextRock(iteration, yPos);
+
+        int consumed = 0;
+        while (ourRock.All(r => !rocks.Contains(r)))
+        {
+            int xOffset = (jets[(index + consumed++) % jets.Length] == '>' ? 1 : -1);
+            if (ourRock.All(r => (r.Item1 + xOffset) is >= 0 and <= 6) &&
+                ourRock.All(r => !rocks.Contains((r.Item1 + xOffset, r.Item2))))
+                ourRock = ourRock.Select(r => (r.Item1 + xOffset, r.Item2 - 1)).ToHashSet();
+            else
+                ourRock = ourRock.Select(r => (r.Item1, r.Item2 - 1)).ToHashSet();
+        }
+
+        // we only stop after intersecting the ground, so move one back up
+        foreach ((long x, long y) in ourRock)
+            rocks.Add((x, y + 1));
+        return consumed;
+    }
+
     public override string Part1(string input)
     {
-        HashSet<(long, long)> rockCells = new HashSet<(long, long)>();
+        HashSet<(long, long)> rocks = new()
+        {
+            (0, 0),
+            (1, 0),
+            (2, 0),
+            (3, 0),
+            (4, 0),
+            (5, 0),
+            (6, 0),
+        };
 
         int jetIndex = 0;
         RescaleBar(2022);
         for (int i = 0; i < 2022; i++)
         {
-            long yPos = rockCells.Count == 0 ? 4 : rockCells.Max(r => r.Item2) + 4;
-            List<(long, long)> ourRock = GetNextRock(i, yPos);
-
-            while (!ourRock.Any(r => r.Item2 == 0 || rockCells.Contains(r)))
-            {
-                bool windRight = input[jetIndex++] == '>';
-                jetIndex %= input.Length;
-
-                List<(long, long)> rockMoved = ourRock.Select(r => (r.Item1 + (windRight ? 1 : -1), r.Item2)).ToList();
-                if (!rockMoved.Any(r => rockCells.Contains(r) || r.Item1 is < 0 or > 6 || r.Item2 <= 0))
-                    ourRock = rockMoved;
-
-                ourRock = ourRock.Select(r => (r.Item1, r.Item2 - 1)).ToList();
-            }
-
-            // we only stop after intersecting the ground, so move one back up
-            foreach ((long x, long y) in ourRock.Select(r => (r.Item1, r.Item2 + 1)))
-                rockCells.Add((x, y));
-
+            jetIndex += DoSimulationIteration(rocks, i, input, jetIndex);
+            jetIndex %= input.Length;
             IncreaseBar();
         }
 
-        return rockCells.Max(r => r.Item2).ToString();
+        return rocks.Max(r => r.Item2).ToString();
     }
 
     public override string Part2(string input)
     {
-        HashSet<(long, long)> rockCells = new();
-
-        Dictionary<(int, long), (long, long)> seen = new();
+        HashSet<(long, long)> rocks = new()
+        {
+            (0, 0),
+            (1, 0),
+            (2, 0),
+            (3, 0),
+            (4, 0),
+            (5, 0),
+            (6, 0),
+        };
         
+        Dictionary<(int, long), (long, long)> seen = new();
+
         long cycleHeight = -1, cycleLength = -1;
 
         long lastAnswer = -1;
-        
+
         int jetIndex = 0;
         for (long i = 0; i < 1000000000000L; i++)
         {
-            long height = rockCells.Count == 0 ? 0 : rockCells.Max(r => r.Item2);
-            long yPos = rockCells.Count == 0 ? 4 : height + 4;
+            long height = rocks.Max(r => r.Item2);
 
             if (seen.ContainsKey((jetIndex, i % 5)))
             {
                 cycleHeight = height - seen[(jetIndex, i % 5)].Item1;
                 cycleLength = i - seen[(jetIndex, i % 5)].Item2;
             }
+
             seen[(jetIndex, i % 5)] = (height, i);
 
             if (cycleHeight > 0 && cycleLength > 0 && (1000000000000L - i) % cycleLength == 0)
@@ -113,25 +136,10 @@ public class Year2022Day17 : Solution
                 lastAnswer = answer;
             }
 
-            List<(long, long)> ourRock = GetNextRock(i % 5, yPos);
-
-            while (!ourRock.Any(r => r.Item2 == 0 || rockCells.Contains(r)))
-            {
-                bool windRight = input[jetIndex++] == '>';
-                jetIndex %= input.Length;
-
-                List<(long, long)> rockMoved = ourRock.Select(r => (r.Item1 + (windRight ? 1 : -1), r.Item2)).ToList();
-                if (!rockMoved.Any(r => rockCells.Contains(r) || r.Item1 is < 0 or > 6 || r.Item2 <= 0))
-                    ourRock = rockMoved;
-
-                ourRock = ourRock.Select(r => (r.Item1, r.Item2 - 1)).ToList();
-            }
-
-            // we only stop after intersecting the ground, so move one back up
-            foreach ((long x, long y) in ourRock)
-                rockCells.Add((x, y+1));
+            jetIndex += DoSimulationIteration(rocks, i, input, jetIndex);
+            jetIndex %= input.Length;
         }
 
-        return rockCells.Max(r => r.Item2).ToString();
+        return rocks.Max(r => r.Item2).ToString();
     }
 }
