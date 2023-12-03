36586e75d6052ab1606df2ddb9b496e5f819280e
T Brieger <tilman.brieger@gmx.de>
2023-12-03 13:35:32 +0100

    2023 days 2/3

---
+++ AdventOfCode/Solutions/2023/Year2023Day02.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace AdventOfCode.Solutions;
+
+public class Year2023Day02 : Solution
+{
+    public override string Part1(string input)
+    {
+        string[] lines = input.Split('\n');
+
+        int idsum = 0;
+        
+        foreach (string line in lines)
+        {
+            int id = int.Parse(line.Split(':')[0].Split(' ')[1]);
+            string[] reveals = line.Split(':', 2)[1].Split(';');
+            bool possible = true;
+            foreach (string reveal in reveals)
+            {
+                string[] individual = reveal.Split(", ");
+                foreach (string indiv in individual)
+                {
+                    string[] split = indiv.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    int c = int.Parse(split[0]);
+                    switch (split[1])
+                    {
+                        case "red":
+                            if (c > 12)
+                                possible = false;
+                            break;
+                        case "blue":
+                            if (c > 14)
+                                possible = false;
+                            break;
+                        case "green":
+                            if (c > 13)
+                                possible = false;
+                            break;
+                    }
+                }
+            }
+
+            if (possible)
+                idsum += id;
+        }
+
+        return idsum.ToString();
+    }
+
+    public override string Part2(string input)
+    {
+        string[] lines = input.Split('\n');
+
+        int sum = 0;
+        
+        foreach (string line in lines)
+        {
+            int id = int.Parse(line.Split(':')[0].Split(' ')[1]);
+            string[] reveals = line.Split(':', 2)[1].Split(';');
+            int r = 0, g = 0, b = 0;
+            foreach (string reveal in reveals)
+            {
+                string[] individual = reveal.Split(", ");
+                foreach (string indiv in individual)
+                {
+                    string[] split = indiv.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    int c = int.Parse(split[0]);
+                    switch (split[1])
+                    {
+                        case "red":
+                            r = Math.Max(c, r);
+                            break;
+                        case "blue":
+                            b = Math.Max(c, b);
+                            break;
+                        case "green":
+                            g = Math.Max(c, g);
+                            break;
+                    }
+                }
+            }
+
+            sum += r * g * b;
+        }
+
+        return sum.ToString();
+    }
+}
---
+++ AdventOfCode/Solutions/2023/Year2023Day03.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Solutions;
+
+public class Year2023Day03 : Solution
+{
+    public override string Part1(string input)
+    {
+        string[] lines = input.Split('\n');
+        HashSet<(int, int)> symbols = new();
+        for (int y = 0; y < lines.Length; y++)
+        {
+            for (int x = 0; x < lines[y].Length; x++)
+            {
+                if (lines[y][x] != '.' && lines[y][x] is < '0' or > '9')
+                {
+                    symbols.Add((x, y));
+                }
+            }
+        }
+
+        (int, int)[] offsets =
+        {
+            (-1, -1), (0, -1), (1, -1),
+            (-1, 0), (1, 0),
+            (-1, 1), (0, 1), (1, 1)
+        };
+
+        int partNumberSum = 0;
+
+        for (int y = 0; y < lines.Length; y++)
+        {
+            int currentNum = 0;
+            bool isAdjacent = false;
+
+            for (int x = 0; x < lines[y].Length; x++)
+            {
+                if (lines[y][x] is >= '0' and <= '9')
+                {
+                    foreach ((int ox, int oy) in offsets)
+                    {
+                        if (symbols.Contains((x + ox, y + oy)))
+                            isAdjacent = true;
+                    }
+
+                    currentNum *= 10;
+                    currentNum += lines[y][x] - '0';
+                }
+                else
+                {
+                    if (isAdjacent)
+                        partNumberSum += currentNum;
+                    currentNum = 0;
+                    isAdjacent = false;
+                }
+            }
+
+            if (isAdjacent)
+                partNumberSum += currentNum;
+        }
+
+        return partNumberSum.ToString();
+    }
+
+    public override string Part2(string input)
+    {
+        string[] lines = input.Split('\n');
+
+        (int, int)[] offsets =
+        {
+            (-1, -1), (0, -1), (1, -1),
+            (-1, 0), (1, 0),
+            (-1, 1), (0, 1), (1, 1)
+        };
+
+        Dictionary<(int, int), int> numbersById = new();
+        Dictionary<int, int> idToValue = new();
+
+        for (int y = 0; y < lines.Length; y++)
+        {
+            int currentNum = 0;
+            int len = 0;
+
+            for (int x = 0; x < lines[y].Length; x++)
+            {
+                if (lines[y][x] is >= '0' and <= '9')
+                {
+                    currentNum *= 10;
+                    currentNum += lines[y][x] - '0';
+                    len++;
+                }
+                else
+                {
+                    for (int i = 0; i < len; i++)
+                    {
+                        numbersById.Add((x - 1 - i, y), idToValue.Count);
+                    }
+
+                    idToValue[idToValue.Count] = currentNum;
+
+                    currentNum = 0;
+                    len = 0;
+                }
+            }
+
+            if (len == 0) continue;
+
+
+            for (int i = 0; i < len; i++)
+            {
+                numbersById.Add((lines[y].Length - 1 - i, y), idToValue.Count);
+            }
+
+            idToValue[idToValue.Count] = currentNum;
+        }
+
+        long gearRatioSum = 0;
+        for (int y = 0; y < lines.Length; y++)
+        {
+            for (int x = 0; x < lines[y].Length; x++)
+            {
+                if (lines[y][x] == '*')
+                {
+                    HashSet<int> numbersAdjacent = new();
+                    foreach ((int ox, int oy) in offsets)
+                    {
+                        if (numbersById.ContainsKey((x + ox, y + oy)))
+                            numbersAdjacent.Add(numbersById[(x + ox, y + oy)]);
+                    }
+
+                    if (numbersAdjacent.Count == 2)
+                        gearRatioSum += numbersAdjacent.Select(i => idToValue[i]).Aggregate(1, (a, b) => a * b);
+                }
+            }
+        }
+        
+        return gearRatioSum.ToString();
+    }
+}
